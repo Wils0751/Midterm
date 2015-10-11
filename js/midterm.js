ce697a4af5d27b7730f34bc575cb0bdc558e5f9e
Shannon Wilson <wils0751@algonquinlive.com>
2015-10-10 21:38:58 -0400

    Midterm

--- js/midterm.js
+++ js/midterm.js
@@ -1,1 +1,16 @@
 //All your JS code goes here
+window.onload =function();
+ alert(" The page has loaded")
+var loadbutton
+
+document.querySelector("#loadBtn").addEventListener("click",function();
+$.getJSON("user.json",function(result){
+    $.each(result,function(i, field){
+        $("div").append(field + " ");
+    })
+});
+
+ 
+    
+    
+    
