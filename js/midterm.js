64d00621146c15f9c909b54dbc0aa1e94956bf10
Shannon Wilson <wils0751@algonquinlive.com>
2015-10-15 13:08:38 -0400

    Changes!!

--- js/midterm.js
+++ js/midterm.js
@@ -1,81 +1,84 @@
 //All your JS code goes here
-document.addEventListener("DOMContentLoaded", function);
-var     newdata= document.querySelector("#output1"),
-        olddata=document.querySelector("#output2"),
-        loadbutton=document.querySelector("#loadBtn"),
-        morebutton=document.querySelector("#showBtn");
+var JSONdata ={};
+var loadbutton ={};
+var morebutton ={};
+var counter = 0;
+var totalsize;
+var tempFlag =0; 
+var JSonfile = "https://raw.githubusercontent.com/Wils0751/Midterm/gh-pages/js/users.json";
+ 
+
+document.addEventListener("DOMContentLoaded", event);
+var newdata=document.querySelector("#output1"),
+    olddata=document.querySelector("#output2"),
+    loadbutton=document.querySelector("#loadBtn"),
+    morebutton=document.querySelector("#showBtn");
 
 
 
-var JSONdata =[],
-    counter = 0,
-    JSonfile = "https://raw.githubusercontent.com/joellord/users/master/users.json"
-
-
-
-loadbutton.addEventListener("click",button);//Click event
-
-function button(){
-
-  loadbutton.classList.remove("enabled");
-  loadbutton.classList.add("disabled");
-  morebutton.classList.remove("disabled");
-  morebutton.classList.add("enabled");
-  nextdata (JSonfile);
-  loaddata.removeEventListener("click",button);
-  morebutton.addEventListener("click",nextdata);
+loadbutton.addEventListener('click', loaddata);
+morebutton.addEventListener('click', next);
     
-}
-
-function Showdata(){
-    
-
 function loaddata(){
-        var requ = new XMLHttpRequest();
-        req.open('GET', file, false); 
+        var req = new XMLHttpRequest();
+        req.open('GET', JSonfile, false); 
         req.send(); 
         req.onreadystatechange = function( )
         { 
             if(req.readyState == 4){
                 if ( req.status == 200)
             {
-                    JSONdata = JSON.parse( req.responseText );
+                    parseJson(req.responseText );
                 }
             }
-        }
-    req.send(null)
+        } {
+        req.send(null)
     }
-function nextdata() //This is for scrolling through the data.  Easy stuff.
-{
-    var index, oldboxtext, con_temp;//In order: An index for scrolling through archived data, the data for the archived data output, a variable for legibility.
-    if (counter_index < counter.length)
-    {
-        con_temp=counter[counter_index];
-        newinfo.innerHTML="<img src='"+con_temp.image+"'\><h2>"+nameparse(con_temp.firstName,con_temp.lastName)+"</h2><a href='mailto://"+con_temp.email+"'>"+con_temp.email+"</a>";//Writing new data to the new data box.
-        oldboxtext="";
-        for (var index=counter_index-3;index<counter_index;index++) //Looping through the three previous name entries.
-        {
-            if (index >= 0)//The first few boxes could have an array index in the negatives, so skip those.
-            {
-                con_temp=counter[index];//Just to make the next line a little cleaner to read.
-                oldboxtext=oldboxtext+"<div><img src='"+con_temp.thumbnail+"'\><a href='mailto://"+con_temp.email+"'>"+nameparse(con_temp.firstName,con_temp.lastName)+"</a></div>";
-            }
-        }
-        olddata.innerHTML=oldboxtext;//OuTPUT to old box.
-        if(counter_index==0)//After the first clickthrough, let's change the name.
-        {
-            nextbutton.innerHTML="Show Next";
-        }
-        counter_index++;
     }
-    else
-    {
-        morebutton.classList.add("btn disabled");
-        morebutton.classList.remove("btn enabled");
-    }
+
+function parseJson(obj) {
+    JSONdata = JSON.parse(obj);
+    totalsize =JSONdata.length;
+    loadbutton.className = "btn disabled";
+    loadbutton.removeEventListener('click', loaddata);
+    morebutton.className = "btn enabled";
 }
 
-function nameparse(first, last)//Breaking up converting the names to proper name case; broken out here simply for legibility.
-{
-    return first.substring(0,1).toUpperCase()+first.substring(1,first.length)+" "+last.substring(0,1).toUpperCase()+last.substring(1,last.length);
+function next() {
+    if (morebutton.innerHTML != 'Show Next') {
+        console.log("written show next");
+        morebutton.innerHTML = 'Show Next';
+    }
+
+    if (counter < totalsize) {
+        setFeedByIndex(currentIndex);
+    }
+    if (counter=== (totalsize - 1)) {
+        morebutton.removeEventListener('click', next);
+        alert('Its End! Refresh the page to see it again.');
+    }
+    counter++;
+    console.log("next()");
 }
+function setFeedByIndex(index) {
+    newdata.innerHTML = '<img src="' + JSONdata[index]['image'] + '"><h2>' + toTitleCase(JSONdata[index]['firstName']) + ' ' + toTitleCase(JSONdata[index]['lastName']) + '</h2><a href="mailto:' + JSONdata[index]['email'] + '">' + JSONdata[index]['email'] + '</a></div>';
+    if (counter != 0) {
+        var data = olddata.innerHTML;
+        console.log(tempFlag);
+        data = data + '<div class="oldData"><div><img src="' + JSONdata[index - 1]['thumbnail'] + '"><a href="mailto:' + JSONdata[index - 1]['email'] + '">' + toTitleCase(JSONdata[index - 1]['firstName']) + ' ' + toTitleCase(JSONdata[index - 1]['lastName']) + '</a></div></div>';
+        olddata.innerHTML = data;
+    }
+    if (tempFlag > 2) {
+       olddata.removeChild(olddata.childNodes[1]);
+    }
+    tempFlag++;
+}
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+
+//http://stackoverflow.com/questions/4878756/javascript-how-to-capitalize-first-letter-of-each-word-like-a-2-word-city
