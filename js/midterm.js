53711a295ea115b1ffab4ff991d0dc7a358f5031
Shannon Wilson <wils0751@algonquinlive.com>
2015-10-16 11:39:26 -0400

    Working code!!

--- js/midterm.js
+++ js/midterm.js
@@ -1,39 +1,38 @@
 //All your JS code goes here
+var JSonfile = "https://raw.githubusercontent.com/Wils0751/Midterm/gh-pages/js/users.json";
 var JSONdata ={};
 var loadbutton ={};
 var morebutton ={};
 var counter = 0;
 var totalsize;
 var tempFlag =0; 
-var JSonfile = "https://raw.githubusercontent.com/Wils0751/Midterm/gh-pages/js/users.json";
- 
+var newdata = document.getElementById("output1");
+var olddata = document.getElementById("output2");
 
-document.addEventListener("DOMContentLoaded", event);
-var newdata=document.querySelector("#output1"),
-    olddata=document.querySelector("#output2"),
-    loadbutton=document.querySelector("#loadBtn"),
-    morebutton=document.querySelector("#showBtn");
-
-
-
-loadbutton.addEventListener('click', loaddata);
-morebutton.addEventListener('click', next);
+document.addEventListener("DOMContentLoaded", function(event){
+    loadbutton = document.getElementById("loadBtn"),
+    morebutton = document.getElementById("showBtn");
+    loadbutton.addEventListener("click", loaddata);
+    morebutton.addEventListener("click", next);
+});
     
-function loaddata(){
-        var req = new XMLHttpRequest();
-        req.open('GET', JSonfile, false); 
-        req.send(); 
-        req.onreadystatechange = function( )
-        { 
-            if(req.readyState == 4){
-                if ( req.status == 200)
-            {
-                    parseJson(req.responseText );
-                }
+function loaddata() {
+    console.log("loaddata()");
+    var req = new XMLHttpRequest();
+    req.open('GET', JSonfile, false);
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                //users.json is fetched
+                parseJson(req.responseText);
             }
-        } 
-        req.send(null)
+        }
     }
+    if (loadbutton.className === 'btn enabled') {
+        req.send(null);
+    }
+
+}
 
 function parseJson(obj) {
     JSONdata = JSON.parse(obj);
@@ -49,7 +48,7 @@ function next() {
     }
 
     if (counter < totalsize) {
-        setFeedByIndex(currentIndex);
+        setFeedByIndex(counter);
     }
     if (counter=== (totalsize - 1)) {
         morebutton.removeEventListener('click', next);
@@ -59,8 +58,10 @@ function next() {
     console.log("next()");
 }
 function setFeedByIndex(index) {
+    var newdata = document.getElementById("output1");
     newdata.innerHTML = '<img src="' + JSONdata[index]['image'] + '"><h2>' + toTitleCase(JSONdata[index]['firstName']) + ' ' + toTitleCase(JSONdata[index]['lastName']) + '</h2><a href="mailto:' + JSONdata[index]['email'] + '">' + JSONdata[index]['email'] + '</a></div>';
     if (counter != 0) {
+        var olddata = document.getElementById("output2");
         var data = olddata.innerHTML;
         console.log(tempFlag);
         data = data + '<div class="oldData"><div><img src="' + JSONdata[index - 1]['thumbnail'] + '"><a href="mailto:' + JSONdata[index - 1]['email'] + '">' + toTitleCase(JSONdata[index - 1]['firstName']) + ' ' + toTitleCase(JSONdata[index - 1]['lastName']) + '</a></div></div>';
